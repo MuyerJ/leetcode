e1f405bd266bda477c3480831ece462d4c748404
叶江 <1140549742@qq.com>
2022-08-11 18:48:11 +0800

    构建树

--- study-demo/src/main/java/com/muyer/node/TreeNode.java
+++ study-demo/src/main/java/com/muyer/node/TreeNode.java
@@ -1,5 +1,8 @@
 package com.muyer.node;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *@描述
  *@创建人 yejiang
@@ -24,5 +27,68 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = buildTree(new Integer[]{1, null, 2, 3, 4, 5});
+        //System.out.println(treeNode);
+    }
+
+    /**
+     * 数组创建二叉树
+     * @param array
+     * @return
+     */
+    public static TreeNode buildTree(Integer[] array) {
+        List<TreeNode> list = new ArrayList<TreeNode>();      //用一个集合来存放每一个Node
+        for (int i = 0; i < array.length; i++) {
+            TreeNode node = new TreeNode(array[i], null, null);    //创建结点，每一个结点的左结点和右结点为null
+            list.add(node); // list中存着每一个结点
+        }
+        // 构建二叉树
+        if (list.size() > 0) {
+            for (int i = 0; i < array.length / 2 - 1; i++) {       // i表示的是根节点的索引，从0开始
+                if (list.get(2 * i + 1) != null) {
+                    // 左结点
+                    list.get(i).left = list.get(2 * i + 1);
+                }
+                if (list.get(2 * i + 2) != null) {
+                    // 右结点
+                    list.get(i).right = list.get(2 * i + 2);
+                }
+            }
+            // 判断最后一个根结点：因为最后一个根结点可能没有右结点，所以单独拿出来处理
+            int lastIndex = array.length / 2 - 1;
+            // 左结点
+            list.get(lastIndex).left = list.get(lastIndex * 2 + 1);
+            // 右结点，如果数组的长度为奇数才有右结点
+            if (array.length % 2 == 1) {
+                list.get(lastIndex).right = list.get(lastIndex * 2 + 2);
+            }
+        }
+        return list.get(0);
+    }
+
+    public static TreeNode buildTree() {
+        /**
+         *
+         *       r1
+         *     r2   r3
+         *   r4 r5 r6 r7
+         */
+        TreeNode r1 = new TreeNode(1);
+        TreeNode r2 = new TreeNode(2);
+        TreeNode r3 = new TreeNode(3);
+        TreeNode r4 = new TreeNode(4);
+        TreeNode r5 = new TreeNode(5);
+        TreeNode r6 = new TreeNode(6);
+        TreeNode r7 = new TreeNode(7);
+        r1.left = r2;
+        r1.right = r3;
+        r2.left = r4;
+        r2.right = r5;
+        r3.left = r6;
+        r3.right = r7;
+        return r1;
     }
 }
