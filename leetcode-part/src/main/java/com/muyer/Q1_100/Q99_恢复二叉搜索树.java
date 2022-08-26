a3ed1133d99c0e0323006d3e69c64e34b9cd81e3
叶江 <1140549742@qq.com>
2022-08-26 13:11:36 +0800

    leetcode99 恢复二叉搜索树

---
+++ leetcode-part/src/main/java/com/muyer/Q1_100/Q99_恢复二叉搜索树.java
@@ -0,0 +1,42 @@
+package com.muyer.Q1_100;
+
+import com.muyer.util.TreeNode;
+
+import java.util.*;
+
+/**
+ *@描述
+ *@创建人 yejiang
+ *@创建时间 2022/8/26 13:10
+ */
+public class Q99_恢复二叉搜索树 {
+    public void recoverTree(TreeNode root) {
+        //收集所有节点，并且排好序
+        List<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            list.add(cur.val);
+            if (cur.left != null) {
+                queue.add(cur.left);
+            }
+            if (cur.right != null) {
+                queue.add(cur.right);
+            }
+        }
+        Collections.sort(list);
+        //中序遍历赋值
+        int index = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            root.val = list.get(index++);
+            root = root.right;
+        }
+    }
+}
